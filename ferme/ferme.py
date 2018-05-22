433a0a0f1c6c38c68191427ac5d81005821256ee
Marc Augier <m.augier@me.com>
2018-05-22 19:43:18 +0200

    DAY 17 première étape, commencer à gérer le joueur

--- ferme/ferme.py
+++ ferme/ferme.py
@@ -5,7 +5,7 @@
 Petit jeu de simulation de ferme
 Garanti sans OGM
 
-(c) Marc Augier 2016
+(c) Marc Augier 2018
 m.augier@me.com
 
 Règles du jeu:
@@ -22,3 +22,55 @@ Avec le stock de blé, il peut :
  - Il doit aussi payer des impots (1000 tonnes)
 
 """
+
+terrain = 100
+stock = 1000
+productivite = 2
+semence = 1
+cout = 10
+taxe = 1000
+
+an = 2016
+
+def affiche_statut_ferme(an, terrain,stock):
+    print ("En {}, notre ferme possède {} hectares et {} tonnes de stock de blé.".format(an,terrain, stock))
+
+def jeu_du_fermier():
+    reponse = ""
+    while(reponse.upper() not in ['O', 'N', 'OUI', 'NON']):
+         reponse = input("Est-ce que vous voulez continuez à jouer ? Oui/Non => ")
+    if reponse.upper() == 'O':
+        decision = {'jeu':True}
+
+    else:
+        decision = {'jeu':False}
+
+    return decision
+
+def execute_simulation(decision):
+    global an, terrain, stock
+    if decision['jeu']:
+        print("ça joue")
+        an += 1
+    return
+
+def verifie_jeu_en_cours(decision):
+    return decision['jeu']
+
+
+print("===============")
+print("=== Ferme 2.0 ===")
+print("===============")
+
+jeu = True
+
+while jeu:
+    affiche_statut_ferme(an, terrain,stock)
+    decision = jeu_du_fermier()
+    execute_simulation(decision)
+    jeu = verifie_jeu_en_cours(decision)
+
+# Affiche le dernier statut
+
+print("Le jeu est terminé")
+affiche_statut_ferme(an, terrain,stock)
