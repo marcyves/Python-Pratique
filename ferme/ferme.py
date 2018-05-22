b9ed4223336d2cc7a09f610c7b80f73a77745d9c
Marc Augier <m.augier@me.com>
2018-05-22 19:39:13 +0200

    DAY 16 première étape de la ferme, la règle du jeu

--- ferme/ferme.py
+++ ferme/ferme.py
@@ -1,2 +1,24 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+Petit jeu de simulation de ferme
+Garanti sans OGM
+
+(c) Marc Augier 2016
+m.augier@me.com
+
+Règles du jeu:
+--------------
+Un paysan possède au départ :
+ - un terrain de 100 ha.
+ - un stock de 1000 tonnes de blé.
+
+Tous les ans, il plante du blé, et son terrain lui produit 2 fois ce qu'il a planté
+Avec le stock de blé, il peut :
+ - Le conserver.
+ - Le planter dans la limite du terrain disponible (1 t/ha).
+ - Acheter plus de terrain (10 t/ha)
+ - Il doit aussi payer des impots (1000 tonnes)
+
+"""
